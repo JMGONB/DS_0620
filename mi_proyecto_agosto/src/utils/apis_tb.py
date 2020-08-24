4b1013fcfd0b624d6b11feaab329cec34b29ae0a
JMGONB <glezblasco68@gmail.com>
2020-08-24 10:12:48 +0200

    Mi proyecto actualizado 24-08-2020 at 10:12

--- mi_proyecto_agosto/src/utils/apis_tb.py
+++ mi_proyecto_agosto/src/utils/apis_tb.py
@@ -1,33 +1,33 @@
-# This module is dedicated to all Apis functions @ Jalex-Yestera
+# Funciones de la API
 
 # ---------
 #  FLASK
 # --------
-app = Flask(__name__)  # init
-@app.route("/")  # Default path
+app = Flask(__name__)  
+@app.route("/")  
 def default():
-    return '''<h1> Api del grupo B</h1> <p> Añadir get_token?id= para conseguir el token, get_json?id= para el json</p>'''
+    return "soy la ruta por defecto. Añadir get_token?id=conseguir token,get_json?id=conseguir json"
 
 @app.route("/get_token", methods = ['GET'])
 def get_token():
     clave = None
-    yeison = {"token": "B23866390994081818446940"}
+    key = {"token": "L51391909"}
     if 'id' in request.args: 
         clave = str(request.args['id'])
-    if clave == 'B145':
-        return yeison['token']
+    if clave == 'L345':
+        return key['token']
     else:
-        return "Error al introducir la clave. Prueba con B145"
+        return "Error al introducir la clave. Prueba nuevamente"
 
 @app.route("/get_json", methods = ['GET'])
 def get_json():
     token = None
-    # Get the settings fullpath
-    settings_file = os.path.dirname(__file__) + "\\..\\..\\resources\\json\\output\\d_averages.json"
-    # Load json from file 
+    # Obtener ruta del archivo
+    settings_file = os.path.dirname(__file__) + "\\..\\..\\resources\\json\\output\\residentes_edad.json"
+    # Carga el json desde el archivo 
     with open(settings_file, "r") as json_file_readed:
-        jason = json.load(json_file_readed)
+        field = json.load(json_file_readed)
     if 'id' in request.args:
         token = str(request.args['id'])
-    if token == 'B23866390994081818446940':
-        return jason
+    if token == 'L51391909':
+        return field
